07ea923aa9e673eb9c83412486cf2d51e56ae774
arcanist <arcanistzed@gmail.com>
2021-08-30 21:01:01 -0400

    feat: use PIXI for HUD to make it always visible

--- scripts/hud.js
+++ scripts/hud.js
@@ -5,8 +5,7 @@ import scsApp from './app.js';
 export default class AttackHUD {
     constructor() {
         Hooks.on("createChatMessage", async app => {
-
-            // Figure out if this is an attack roll
+            /** Whether or not this is an attack roll */
             let isAttack;
             switch (game.system.id) {
                 case "dnd5e":
@@ -17,31 +16,69 @@ export default class AttackHUD {
                     isAttack = type === "attack-roll" || type === "spell-attack-roll";
             };
 
-            // Update HUD if this is an Attack roll and the feature is enabled
-            if (isAttack && game.settings.get(scsApp.ID, "showAttackHUD")) {
-                console.log("SCS| Latest attack roll set:", app.roll.total);
+            // Update HUD if this is an Attack roll, the feature is enabled, and it's the Attack phase
+            if (isAttack
+                && game.settings.get(scsApp.ID, "showAttackHUD")
+                && scsApp.phases.names[scsApp.currentPhase - 1] === "Attacks"
+            ) {
+                /** This token's ID */
+                const tokenId = app.data.speaker.token;
 
-                // Set flag on Actor
-                await game.actors.get(app.data.speaker.actor).setFlag(scsApp.ID, "lastAttackRoll", app.roll.total);
+                /** This attack roll */
+                const roll = app.roll.total;
+                console.log("SCS | Latest attack roll", roll);
 
-                // Re-render HUD
-                const token = game.scenes.get(game.canvas.scene.id).tokens.get(app.data.speaker.token).object;
-                canvas.hud.token.bind(token);
+                // Get token placeables for this actor
+                canvas.tokens.placeables.filter(token => token.id === tokenId).forEach(placeable => {
+
+                    // If no PIXI HUD already exist for this token, create one
+                    if (!this.HUD[tokenId]) this.createHUD(tokenId, placeable);
+
+                    // Update text
+                    this.HUD[tokenId].text.text = roll;
+
+                    // When the phase changes, if it's no longer the attack phase, remove the HUD
+                    Hooks.on("scsPhaseChanged", currentPhase => {
+                        if (scsApp.phases.names[currentPhase - 1] !== "Attacks") {
+                            placeable.removeChild(this.HUD[tokenId].icon);
+                            this.HUD[tokenId] = false;
+                        };
+                    });
+                });
             };
         });
 
-        Hooks.on("renderTokenHUD", async (_app, html, data) => {
-            // Get flag on Actor
-            const roll = await game.actors.get(data.actorId).getFlag(scsApp.ID, "lastAttackRoll");
-
-            // Create control
-            const control = document.createElement("div");
-            control.classList.add("control-icon");
-            control.title = "Last Attack Roll";
-            control.innerText = roll;
-
-            // Add to HUD if there is a roll value
-            if (roll) html[0].querySelector(".col.right").append(control);
+        // Remove the previous HUD when the token changes sizes
+        Hooks.on("updateToken", (document, change) => {
+            if (change.width || change.height) this.HUD[document.id] = false;
         });
+    };
+
+    /** The PIXI Objects in the HUD */
+    HUD = {};
+
+    /** Create the HUD */
+    createHUD(tokenId, placeable) {
+        // Create a new PIXI HUD for this token
+        this.HUD[tokenId] = {};
+
+        // Create PIXI Objects
+        const TEXT = this.HUD[tokenId].text = new PIXI.Text("", { fill: 0x000000, fontSize: 125, fontFamily: "Roboto", fontWeight: "bold", align: "center" });
+        const ICON = this.HUD[tokenId].icon = new PIXI.Sprite.from("/icons/svg/combat.svg");
+
+        // Adjust icon dimensions and position
+        ICON.height = 48;
+        ICON.width = 48;
+        ICON.y = game.scenes.active.tokens.get(tokenId).object.height / 1.5;
+        ICON.x = game.scenes.active.tokens.get(tokenId).object.width / 2 - 36;
+
+        // Adjust text resolution and position
+        TEXT.resolution = 10;
+        TEXT.x = 180;
+        TEXT.y = 210;
+
+        // Add to the placeable
+        placeable.addChild(ICON);
+        ICON.addChild(TEXT);
     };
 };
